7168bbcdac72976511bb5ae25d080820858dd6da
xuyang <xuyang@wdai.com>
2018-08-28 21:09:49 +0800

    新增页面布局layout

--- pages/console.jsx
+++ pages/console.jsx
@@ -6,9 +6,25 @@ import "./console.scss"
 export default class Console extends Component {
     render() {
         return <div className="hp-console">
-            <Panel className="m-10" title="数据面板" toolbar={true}>
-                123
-            </Panel>
+            <div className="hp-sp-row">
+                <div className="hp-col hp-col-24">
+                    <Panel className="mv-10" title="数据面板" toolbar={true}>
+                        123
+                    </Panel>
+                </div>
+            </div>
+            <div className="hp-sp-row">
+                <div className="hp-col hp-col-12">
+                    <Panel className="mv-10" title="数据面板" toolbar={true}>
+                        123
+                    </Panel>
+                </div>
+                <div className="hp-col hp-col-12">
+                    <Panel className="mv-10" title="数据面板" toolbar={true}>
+                        123
+                    </Panel>
+                </div>
+            </div>
         </div>
     }
 }
