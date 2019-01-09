08f03de75984c27b5f16ff0cd259bde56527d258
xuyang <xuyang@wdai.com>
2019-01-09 22:29:59 +0800

    新增千分符工具函数

--- components/utils/utils.js
+++ components/utils/utils.js
@@ -61,3 +61,12 @@ export function objectCompare(obj1, obj2) {
     }
     return allSame
 }
+
+// 数字千分符
+function commafy(num){
+    return num && num
+        .toString()
+        .replace(/(\d)(?=(\d{3})+\.)/g, function($1, $2){
+            return $2 + ',';
+        });
+}
